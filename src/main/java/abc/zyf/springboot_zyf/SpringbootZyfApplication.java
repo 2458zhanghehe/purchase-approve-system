24728cf56ba4e5815e59568e375cec89ed318d46
2458zhanghehe <2458050004@qq.com>
2025-09-18 21:21:59 +0800

    first commit

---
+++ src/main/java/abc/zyf/springboot_zyf/SpringbootZyfApplication.java
@@ -0,0 +1,13 @@
+package abc.zyf.springboot_zyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZyfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZyfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/abc/zyf/springboot_zyf/SpringbootZyfApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.zyf.springboot_zyf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZyfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
